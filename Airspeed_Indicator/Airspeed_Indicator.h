1a22e92526a4420be1550261b3d6c8ed4fe8db07
Save Sabanal <save.sabanal@gmail.com>
2024-08-16 21:33:03 -0500

    First release

---
+++ Airspeed_Indicator/Airspeed_Indicator.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "Arduino.h"
+
+class Airspeed_Indicator
+{
+public:
+    Airspeed_Indicator(uint8_t Pin1, uint8_t Pin2);
+    void begin();
+    void attach(uint16_t Pin3, char *init);
+    void detach();
+    void set(int16_t messageID, char *setPoint);
+    void update();
+
+private:
+    bool    _initialised;
+    uint8_t _pin1, _pin2, _pin3;
+    float scaleValue(float x, float in_min, float in_max, float out_min, float out_max);
+    void drawASIGauge();
+    void setInstrumentBrightnessRatio(float ratio);
+    void setAirspeed(float value);
+    void setPowerSave(bool enabled);
+
+    // Variables
+    float startTime;
+    float endTime;
+    float trueAirSpeed = 0;
+    uint16_t altitude = 0;
+    float verticalAirSpeed = 0;
+    float baroPressure = 0;
+    float instrumentBrightness = 1;
+    float instrumentBrightnessRatio = 0;
+    float ASIneedleRotation = 0; // angle of rotation of needle based on the Indicated AirSpeed
+    float airSpeed = 0;
+    bool powerSaveFlag;
+};
